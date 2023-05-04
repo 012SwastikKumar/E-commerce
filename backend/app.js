e59ce7b9cba98044b34f4c59b8874e8b79a51bde
012SwastikKumar <swastiktovisit1234@gmail.com>
2023-05-04 19:12:17 +0530

    order-api-done excluding(email, reviews) ---- Starting frontend

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-const errorMiddleware = require('./middleware/error')
+const errorMiddleware = require("./middleware/error");
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 // Route Imports
-const products = require('./routes/productRoute')
-const user = require('./routes/userRoute')
+const products = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
-app.use("/api/v1",products)
-app.use("/api/v1",user)
-
+app.use("/api/v1", products);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // Middlewares for error handling
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-
-
-
-module.exports=app
+module.exports = app;
